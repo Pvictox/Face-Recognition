df6eb183aa55763b9a5629f0e9d61ca88c98c978
Emanuel Lindoso Perreira <emanuelstrato999@gmail.com>
2022-10-03 21:38:58 -0300

    first commit

---
+++ face_recognition copy.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+import dlib
+
+detector = dlib.get_frontal_face_detector()
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector(img, 1)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]  
+    cv2.imshow('video',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
